65d44c0b81bc3428e23e029b6cd29c9f19da66dc
tensae7654 <89252352+tensae7654@users.noreply.github.com>
2022-10-15 13:32:29 +0300

    Create Valid Parentheses.py

---
+++ Valid Parentheses.py
@@ -0,0 +1,51 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack=[]
+        check= False
+        
+        for x in range(len(s)):
+            p= s[x]
+            if len(s)%2 ==0:
+                if p== '(' or p== '[' or p== '{':
+                    stack.append(p)
+
+                else:
+
+                    if stack:
+                        y= stack.pop()
+                        if p== ')':
+                            if y == '(':
+                                check = True
+                            else:
+                                check = False
+                                return(check)
+
+                        elif p== ']':
+                            if y == '[':
+                                check = True
+                            else:
+                                check = False
+                                return(check)
+                        elif p== '}':
+                            if y == '{':
+                                check = True
+                            else:
+                                check = False
+                                return(check)
+                    else:
+                        check= False
+                        return(check)
+            else:
+                 return(check)
+        if not s:  
+            return(check)
+        else:
+            return False
+            
+        
+        
+        
